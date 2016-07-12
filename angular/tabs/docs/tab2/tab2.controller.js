aafaaec4ee11532d205b9a4d3df509ad6e0fb0b9
Jeffery <j306796405@163.com>
2016-07-12 17:31:52 +0800

    add tabs

---
+++ angular/tabs/docs/tab2/tab2.controller.js
@@ -0,0 +1,17 @@
+/**
+ * FileName tab1.controller
+ * Date 2016-07-12
+ * Created by jianglinj
+ */
+(function(){
+    angular.module('tabsDemo').controller('tab2Controller', function ($scope, $state) {
+        $scope.datas = {
+            title: 123,
+            value: 'tab2'
+        }
+
+        $scope.go = function(){
+            $state.go('main.subTab2');
+        }
+    });
+})()
